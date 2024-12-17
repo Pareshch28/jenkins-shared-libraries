ea72c80f3fdaad2ce0c64e3445f2320489c2a6e8
Paresh ch <144373102+Pareshch28@users.noreply.github.com>
2024-12-17 13:33:18 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(String Projectname ,String Imagetag ,String DockerHubUser){
-  sh "docker build -t DockerHubUser/Projectname:Imagetag ."
-  echo "build successful"
+def call(String Projectname, String Imagetag, String DockerHubUser) {
+    sh "docker build -t ${DockerHubUser}/${Projectname}:${Imagetag} ."
+    echo "Build successful"
 }
